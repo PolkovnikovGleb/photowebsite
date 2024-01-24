483c783faed9d6a9ac8bd04fe56a56bab188ab3e
Gleb Polkovnikov <glebpolkovnikov@MacBook-Pro-Gleb.local>
2024-01-24 13:49:11 +0700

    routesUserAlbumAutn

---
+++ server/config/serverConfig.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+const sessionConfig = {
+  store: new FileStore(),
+  name: 'user_sid', // Имя куки для хранения id сессии. По умолчанию - connect.sid
+  secret: process.env.SESSION_SECRET ?? 'test', // Секретное слово для шифрования, может быть любым
+  resave: false, // Пересохранять ли куку при каждом запросе
+  saveUninitialized: false, // Создавать ли сессию без инициализации ключей в req.session
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 12, // Срок истечения годности куки в миллисекундах
+    httpOnly: true, // Серверная установка и удаление куки, по умолчанию true
+  },
+};
+
+function serverConfig(app) {
+  app.use(cookieParser());
+  app.use(session(sessionConfig));
+  app.use(express.static(path.join(__dirname, '../public')));
+  app.use(express.static(path.join(__dirname, '../../client/build')));
+
+  app.use(express.urlencoded({extended: true}));
+  app.use(express.json());
+  app.use(cors());
+}
+
+module.exports = serverConfig;
