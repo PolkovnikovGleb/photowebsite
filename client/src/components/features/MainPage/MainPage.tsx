fe7233b5ba1eb7312c0c087258d0664043440977
ivo7643 <nahdora001@gmail.com>
2024-01-25 17:09:17 +0700

    Исправил замечания

--- client/src/components/features/MainPage/MainPage.tsx
+++ client/src/components/features/MainPage/MainPage.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function MainPage(): JSX.Element {
   return (
     <div>
-      <div className="Main">
+      <div className="main">
         <NavBar />
       </div>
     </div>
