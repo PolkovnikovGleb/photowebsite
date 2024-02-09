962d690b2daefe2abc71c4a53a68e6c81e47b1cf
Gleb Polkovnikov <glebpolkovnikov@MacBook-Pro-Gleb.local>
2024-02-09 14:34:39 +0700

    create layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,15 @@
-import React, { useEffect } from 'react'
-import { Route, Routes } from 'react-router'
-import { router } from './configs/router'
-import { albumsInit } from './store/Albums/albumsSlice'
-import { useAppDispatch } from './store/store'
+import React, {useEffect} from 'react';
+import {Route, Routes} from 'react-router';
+import {router} from './configs/router';
+import {albumsInit} from './store/Albums/albumsSlice';
+import {useAppDispatch} from './store/store';
 
-function App (): JSX.Element {
-  const dispatch = useAppDispatch()
+function App(): JSX.Element {
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(albumsInit())
-  }, [])
+    dispatch(albumsInit());
+  }, []);
 
   return (
     <div className="app">
@@ -19,7 +19,7 @@ function App (): JSX.Element {
         ))}
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
