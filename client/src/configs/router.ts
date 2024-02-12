8950973b873be021d0dfe88c57e5d54da91dcc79
PolkovnikovGleb <glebpolk@gmail.com>
2024-02-12 23:00:00 +0700

    fix naming

--- client/src/configs/router.ts
+++ client/src/configs/router.ts
@@ -1,30 +1,30 @@
-import Auth from '../pages/auth/AuthPage'
-import Home from '../pages/home/HomePage'
-import Album from '../pages/album/AlbumPage'
+import Auth from '../pages/auth/AuthPage';
+import Home from '../pages/home/HomePage';
+import Album from '../pages/albums/AlbumsPage';
 
 export const routes = {
   home: '/',
   auth: '/auth',
-  albums: '/albums'
-}
+  albums: '/albums',
+};
 
 export const router = [
   {
     id: 1,
     name: 'home',
     path: routes.home,
-    component: Home
+    component: Home,
   },
   {
     id: 5,
     name: 'auth',
     path: routes.auth,
-    component: Auth
+    component: Auth,
   },
   {
     id: 2,
     name: 'albums',
     path: routes.albums,
-    component: Album
-  }
-]
+    component: Album,
+  },
+];
