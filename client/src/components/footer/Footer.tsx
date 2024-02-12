007fa73146c4c46894facab694b84f2865af0dd1
PolkovnikovGleb <glebpolk@gmail.com>
2024-02-12 15:57:53 +0700

    updute scss footer/navbar

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -5,28 +5,34 @@ function Footer(): React.JSX.Element {
   return (
     <div className={s.footer}>
       <div className={s.footer__vk}>
-        <img
-          className={s.vk__img}
-          src="https://cdn.icon-icons.com/icons2/3717/PNG/512/vk_social_media_logo_brand_business_icon_230291.png"
-          alt="icon"
-        />
-        <a href="http://vk.com/glebpolk"> VK</a>
+        <a href="http://vk.com/glebpolk">
+          <img
+            className={s.vk__img}
+            src="https://cdn.icon-icons.com/icons2/3717/PNG/512/vk_social_media_logo_brand_business_icon_230291.png"
+            alt="icon"
+          />
+          VK
+        </a>
       </div>
       <div className={s.footer__inst}>
-        <img
-          className={s.inst__img}
-          src="https://cdn.icon-icons.com/icons2/3717/PNG/512/photo_social_media_logo_facebook_instagram_photography_phot_icon_230308.png"
-          alt="icon"
-        />
-        <a href="http://www.instagram.com/glebpol">IG</a>
+        <a href="http://www.instagram.com/glebpol">
+          <img
+            className={s.inst__img}
+            src="https://cdn.icon-icons.com/icons2/3717/PNG/512/photo_social_media_logo_facebook_instagram_photography_phot_icon_230308.png"
+            alt="icon"
+          />
+          IG
+        </a>
       </div>
       <div className={s.footer__tg}>
-        <img
-          className={s.tg__img}
-          src="https://cdn.icon-icons.com/icons2/3717/PNG/512/telegram_chat_brand_communication_message_free_icon_230295.png"
-          alt="icon"
-        />
-        <a href="http://telegram.org/@glebpolkovnikov">TG</a>
+        <a href="https://t.me/glebpolkovnikov">
+          <img
+            className={s.tg__img}
+            src="https://cdn.icon-icons.com/icons2/3717/PNG/512/telegram_chat_brand_communication_message_free_icon_230295.png"
+            alt="icon"
+          />
+          TG
+        </a>
       </div>
     </div>
   );
