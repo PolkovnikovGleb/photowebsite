67c914aa043d3969e279b1ed419266657b2e9dd9
PolkovnikovGleb <glebpolk@gmail.com>
2024-02-13 12:57:13 +0700

    update scss footer

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import s from './styles/style.module.scss';
 import {socialMedia} from '../../configs/socialMedia';
-import {Link} from 'react-router-dom';
 
 function Footer(): React.JSX.Element {
   return (
     <div className={s.footer}>
       {socialMedia.map((item) => (
         <a href={item.link}>
-          <img className={s.vk__img} src={item.imglink} alt="icon" />
+          <img className={s.footer__img} src={item.imglink} alt="icon" />
           {item.linkname}
         </a>
       ))}
