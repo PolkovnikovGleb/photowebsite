fd2067403d50669e43369348b1d2380a83e33856
PolkovnikovGleb <glebpolk@gmail.com>
2024-02-13 18:25:32 +0700

    update homePage

---
+++ client/src/configs/services.ts
@@ -0,0 +1,20 @@
+export const services = [
+  {
+    nameImg:
+      'https://sun9-8.userapi.com/impf/kyWmEG8azgEkNp2spZ3A7qqd4VbDA7iYLflhPA/dF8AB5E3UyQ.jpg?size=1728x2160&quality=95&sign=31965c80ae05e28df54e0de4f0d69709&type=album',
+    title: 'Индивидуальная съeмка',
+    description: 'Съемка для себя любимой',
+  },
+  {
+    nameImg:
+      'https://sun9-72.userapi.com/impf/c841239/v841239063/639c9/HtStrM2AHTM.jpg?size=640x800&quality=96&sign=a78601abbab7b51541071c7273a22aaf&type=album',
+    title: 'Предметная съeмка',
+    description: 'Съемка для Ozon и WB',
+  },
+  {
+    nameImg:
+      'https://sun9-19.userapi.com/impf/c830308/v830308288/c31d8/OkxM99oJZ18.jpg?size=1728x2160&quality=96&sign=d815d14b0766e514dcbc25c20e2b7cf0&type=album',
+    title: 'Коммерческая съeмка',
+    description: 'Для брендов/одежы/каталога',
+  },
+];
--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import s from './styles/style.module.scss';
+import {services} from '../../configs/services';
 
 function HomePage(): React.JSX.Element {
   return (
@@ -23,33 +24,17 @@ function HomePage(): React.JSX.Element {
         </div>
       </div>
       <div className={s.home__price}>
-        <div className={s.price__one}>
-          <img
-            className={s.price__img}
-            src="https://sun9-8.userapi.com/impf/kyWmEG8azgEkNp2spZ3A7qqd4VbDA7iYLflhPA/dF8AB5E3UyQ.jpg?size=1728x2160&quality=95&sign=31965c80ae05e28df54e0de4f0d69709&type=album"
-            alt="photoprice1"
-          />
-          <h5>Индивидуальная съeмка</h5>
-          <p>Съемка для себя любимой</p>
-        </div>
-        <div className={s.price__two}>
-          <img
-            className={s.price__img}
-            src="https://sun9-72.userapi.com/impf/c841239/v841239063/639c9/HtStrM2AHTM.jpg?size=640x800&quality=96&sign=a78601abbab7b51541071c7273a22aaf&type=album"
-            alt="photoprice2"
-          />
-          <h5>Предметная съeмка</h5>
-          <p>Съемка для Ozon и WB</p>
-        </div>
-        <div className={s.price__three}>
-          <img
-            className={s.price__img}
-            src="https://sun9-19.userapi.com/impf/c830308/v830308288/c31d8/OkxM99oJZ18.jpg?size=1728x2160&quality=96&sign=d815d14b0766e514dcbc25c20e2b7cf0&type=album"
-            alt="photoprice3"
-          />
-          <h5>Коммерческая съeмка</h5>
-          <p>Для брендов/одежы/каталога</p>
-        </div>
+        {services.map((item) => (
+          <div>
+            <img
+              className={s.price__img}
+              src={item.nameImg}
+              alt="photoprice2"
+            />
+            <h5>{item.title}</h5>
+            <p>{item.description}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
